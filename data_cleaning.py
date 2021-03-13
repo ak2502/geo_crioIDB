5870798a483e06752ce4378bc0c7a90a2f669732
Akanksha <akanksha.feb25@.com>
2021-03-13 20:32:52 +0530

    cleaned data

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,3 +6,4 @@ df = pd.read_csv("food_coded.csv")
 df1=df[["GPA","Gender","eating_out","employment","exercise","grade_level","on_off_campus","pay_meal_out","self_perception_weight","sports"]]
 
 df1.to_csv("food_choices.csv")
+#print(df1)
