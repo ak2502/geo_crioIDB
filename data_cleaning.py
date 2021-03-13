e74f6206074089b89d338c63ec050b4f9686bf50
Akanksha <akanksha.feb25@.com>
2021-03-13 20:08:23 +0530

    data cleaning code added

---
+++ data_cleaning.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import pandas as pd 
+
+df = pd.read_csv("food_coded.csv")
+
+df1=df[["GPA","Gender","eating_out","employment","exercise","grade_level","on_off_campus","pay_meal_out","self_perception_weight","sports"]]
+
+#print(df1)
