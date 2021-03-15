b21600a06b9cd77bf7682e8041d67c70467b4f32
ak2502 <akanksha.feb25@.com>
2021-03-15 17:01:08 +0530

    boxplot modified

--- plotting.py
+++ plotting.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("food_choices.csv")
 
-df.boxplot(column=["Gender","eating_out","employment","exercise","grade_level","on_off_campus","pay_meal_out","self_perception_weight","sports"],grid=False)
+df.boxplot(grid=False)
+#df.boxplot(column=["Gender","eating_out","employment","exercise","grade_level","on_off_campus","pay_meal_out","self_perception_weight","sports"],grid=False)
 
 plt.show()
