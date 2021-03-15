db1ed7e5e8e95616ed2c93299491aa2afb93dd4b
ak2502 <akanksha.feb25@.com>
2021-03-15 20:20:36 +0530

    data cleaning updated

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 df = pd.read_csv("food_coded.csv")
 
-df1=df[["GPA","Gender","eating_out","employment","exercise","grade_level","on_off_campus","pay_meal_out","self_perception_weight","sports"]]
+df1=df[["cook","diet_current_coded","eating_out","employment","exercise","fav_cuisine_coded","on_off_campus","pay_meal_out","fav_food","fruit_day","income"]]
 
 df1.to_csv("food_choices.csv")
 #print(df1)
