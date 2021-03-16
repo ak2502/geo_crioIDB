05e7e6f7a419572d5b34647ad93c80e279e0eeaa
ak2502 <akanksha.feb25@.com>
2021-03-17 00:16:22 +0530

    update clustering.py

--- clustering.py
+++ clustering.py
@@ -1,23 +1,22 @@
 from sklearn.cluster import KMeans
+import random
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt 
 
-df = pd.read_csv("food_coded.csv")
-x = df[["weight","waffle_calories"]]
+df = pd.read_csv("food_choices.csv")[["cook","diet_current_coded","eating_out","sports","exercise","fav_cuisine_coded","on_off_campus","pay_meal_out","fav_food","fruit_day","income"]]
 
+k = 3
+kmeans = KMeans(n_clusters = k, random_state=0).fit(df)
+df['Cluster']=kmeans.labels_
 
-k = 8
+fig, axes = plt.subplots(1, k, sharey=True)
+axes[0].set_ylabel('Clusters', fontsize=20)
 
-Centroids = (x.sample(n = k))
-
-plt.scatter(x["weight"],x["waffle_calories"],c ='b')
-plt.scatter(Centroids["weight"],Centroids["waffle_calories"],c='red')
-
-
-plt.xlabel("Weight")
-plt.ylabel("Waffle Calories")
-plt.show()
-
-df.boxplot(column=["weight","waffle_calories"],grid=False)
+for i in range(k):
+     plt.sca(axes[i])
+     plt.xticks(rotation=45,ha='right')
+     sns.boxplot(palette="Set1",data = df[df['Cluster'] == i].drop('Cluster',1), ax=axes[i]).tick_params(labelsize=8.7)
+        
 plt.show()
