ac5f3cefc0fe55e5afd82f2c236da711c6eff363
ak2502 <akanksha.feb25@.com>
2021-03-16 23:27:43 +0530

    update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,9 +1,10 @@
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv("food_choices.csv")
+df = pd.read_csv("food_choices.csv")[["cook","diet_current_coded","eating_out","sports","exercise","fav_cuisine_coded","on_off_campus","pay_meal_out","fav_food","fruit_day","income"]]
 
-df.boxplot(column=["cook","diet_current_coded","eating_out","employment","exercise","fav_cuisine_coded","on_off_campus","pay_meal_out","fav_food","fruit_day","income"],grid=False)
-
+sns.boxplot(data=df, palette="Set1").tick_params(labelsize=8.7)
+plt.xticks(rotation=45, ha='right')
 plt.show()
